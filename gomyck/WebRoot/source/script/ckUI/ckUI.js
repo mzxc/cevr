32d1fe21e566bfd59cc8971653db99f20dc3a34a
medness <medness@192.168.1.166>
2017-04-08 16:15:34 +0800

    Signed-off-by: medness <medness@192.168.1.166>

--- gomyck/WebRoot/source/script/ckUI/ckUI.js
+++ gomyck/WebRoot/source/script/ckUI/ckUI.js
@@ -895,7 +895,19 @@ $.extend({
 		result.push(obj);
 		result.push(false);
 		return result;
-	}
+	},
+	ckPostToNewView:function(url,args){
+        var form = $("<form method='post'></form>"),
+            input;
+        form.attr({"action":url});
+        $.each(args,function(key,value){
+            input = $("<input type='hidden'>");
+            input.attr({"name":key});
+            input.val(value);
+            form.append(input);
+        });
+        form.submit();
+    }
 });
 //$.ckAnchor();
 })(jQuery);
