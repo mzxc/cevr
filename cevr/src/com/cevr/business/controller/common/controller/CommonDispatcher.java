99eb3ed717a111393142ee6a66cb98ba209a2a32
unknown <haocm@conac.cn>
2017-06-11 18:03:57 +0800

    增加首页，增加视频播放页

--- cevr/src/com/cevr/business/controller/common/controller/CommonDispatcher.java
+++ cevr/src/com/cevr/business/controller/common/controller/CommonDispatcher.java
@@ -10,12 +10,20 @@
  */
 package com.cevr.business.controller.common.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cevr.business.interceptor.LogInfo;
+import com.cevr.business.model.to.TicketInfo;
 
 /**
  * 通用转发器
@@ -36,4 +44,5 @@ public class CommonDispatcher
     {
         return view + "/" + page;
     }
+
 }
--- cevr/src/com/cevr/business/controller/welcome/controller/IndexHandler.java
+++ cevr/src/com/cevr/business/controller/welcome/controller/IndexHandler.java
@@ -14,10 +14,13 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -53,7 +56,15 @@ public class IndexHandler {
 		List<Map<String, Object>> searchCarInfo = iis.searchCarInfo(ti);
 		return ResultBuild.init(true, "查询成功", searchCarInfo);
 	}
-	
+//	@LogInfo(operateModelNm="投票首页", operateFuncNm="访问首页")
+//	@RequestMapping(value="tab/{tab}", method=RequestMethod.GET)
+//	public String tab(@PathVariable String tab){
+//		System.out.println(tab);
+//		//List<Map<String, Object>> searchCarInfo = iis.searchCarInfo(ti);
+//    	
+//		//response.sendRedirect(request.getContextPath() + "/common/forward/main/mainframe");
+//		return "main/exteriorDesign";
+//	}
 	@LogInfo(operateModelNm="投票首页", operateFuncNm="新增投票")
 	@RequestMapping(value="ticketCar", method=RequestMethod.POST)
 	@ResponseBody
@@ -63,7 +74,23 @@ public class IndexHandler {
 		ResultMessage rm = iis.addTicketInfo(ti);
 		return ResultBuild.init(rm.isIfSuccess(), rm.getMsg(), rm);
 	}
-	
+    
+    @LogInfo(operateModelNm = "投票首页", operateFuncNm = "带参数页面通用跳转")
+    @RequestMapping(value = "voteframe/{tab}", method = RequestMethod.GET)
+    public String gotoVoteframe(HttpServletRequest request, HttpServletResponse response,@PathVariable final String tab)
+    {
+		request.setAttribute("tab", tab);
+        return "main/voteframe";
+    }
+    
+    @LogInfo(operateModelNm = "投票首页", operateFuncNm = "带参数页面通用跳转")
+    @RequestMapping(value = "player/{id}", method = RequestMethod.GET)
+    public String gotoPlayer(HttpServletRequest request, HttpServletResponse response,@PathVariable final String id)
+    {
+		request.setAttribute("src", "http://data.cnlive.com/export/CNLivePlayer.swf?hasBorder=false&amp;uuid=58b6bbf6ddd3426abbeb42c631ac43fe");
+        return "main/playerJsp";
+    }
+    
 	@LogInfo(operateModelNm="投票首页", operateFuncNm="删除投票")
 	@RequestMapping(value="unTicketCar", method=RequestMethod.POST)
 	@ResponseBody
