5e2a0e524d323508906489a107576a8104faf7b2
medness <medness@192.168.1.166>
2017-06-26 14:33:23 +0800

    Signed-off-by: medness <medness@192.168.1.166>

--- gomyck/src/com/gomyck/business/interceptor/JsonConverter.java
+++ gomyck/src/com/gomyck/business/interceptor/JsonConverter.java
@@ -1,19 +1,13 @@
 package com.gomyck.business.interceptor;
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializerProvider;
 
 public class JsonConverter extends ObjectMapper {
     
     public JsonConverter() {
         super();
-        System.out.println("123@@@@");
         // 允许单引号
         this.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
         // 字段和值都加引号
@@ -22,12 +16,12 @@ public class JsonConverter extends ObjectMapper {
         this.configure(JsonGenerator.Feature.WRITE_NUMBERS_AS_STRINGS, true);
         this.configure(JsonGenerator.Feature.QUOTE_NON_NUMERIC_NUMBERS, true);
         // 空值处理为空串
-        this.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
-            @Override
-            public void serialize(Object value, JsonGenerator jg, SerializerProvider sp)
-                throws IOException, JsonProcessingException {
-                jg.writeString("");
-            }
-        });
+        // this.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+        // @Override
+        // public void serialize(Object value, JsonGenerator jg, SerializerProvider sp)
+        // throws IOException, JsonProcessingException {
+        // jg.writeString("");
+        // }
+        // });
     }
 }
